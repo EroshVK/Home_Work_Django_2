09c6b7eb4e78e92c04c2742c3e6983a98b5c839c
EroshVK <635753051vika@gmai.com>
2023-08-18 22:40:46 +0300

    Homework four done

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product
 
 app_name = "catalog"
 
 urlpatterns = [
-    path('', home),
-    path('contacts', contacts)
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contact'),
+    path('product/<int:id>/', product, name='product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render
+from catalog.models import Product
+
 
 # Create your views here.
 
 
 def home(request):
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Главная'
+    }
     if request.method == 'GET':
-        return render(request, 'catalog/home.html')
+        return render(request, 'catalog/home.html', context)
 
 
-def contacts(request) -> object:
+def contacts(request):
+    context = {
+        'title': 'Контакты'
+    }
     if request.method == 'GET':
-        return render(request, 'catalog/contacts.html')
+        return render(request, 'catalog/contacts.html', context)
+
+
+def product(request, id):
+    product_id = Product.objects.get(pk=id)
+    context = {
+        "name": product_id.name,
+        "description": product_id.description,
+        "image": product_id.image,
+        "category": product_id.category,
+        "price": product_id.price,
+        "creation_date": product_id.creation_date,
+        "last_modified_date": product_id.last_modified_date,
+        "title": f"Товар {product_id.name}"
+    }
+    if request.method == 'GET':
+        return render(request=request, template_name='catalog/product.html', context=context)
